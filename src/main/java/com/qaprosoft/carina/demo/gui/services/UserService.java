726ed1ed449443816b560ccbd46ae92c6997cd6a
Anastasiia Shchavinska <ashchavinska@solvd.com>
2021-04-29 21:24:23 +0300

    added logger to services package

--- src/main/java/com/qaprosoft/carina/demo/gui/services/UserService.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/services/UserService.java
@@ -1,9 +1,14 @@
 package com.qaprosoft.carina.demo.gui.services;
 import com.qaprosoft.carina.core.foundation.utils.R;
 import com.qaprosoft.carina.demo.gui.components.User;
+import org.apache.log4j.Logger;
 
 public class UserService {
+
+    private static final Logger LOGGER = Logger.getLogger(LoginService.class);
+
     public User getUser() {
+        LOGGER.info("Get user.");
         User user = new User();
         user.setEmail(R.TESTDATA.get("email"));
         user.setPassword(R.TESTDATA.get("password"));
