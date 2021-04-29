d10564eb3e9eb6b78f3e04539769c3caf5fb666c
Anastasiia Shchavinska <ashchavinska@solvd.com>
2021-04-29 17:37:43 +0300

    added loger to pages package

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/LoginPage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/LoginPage.java
@@ -2,10 +2,13 @@ package com.qaprosoft.carina.demo.gui.pages;
 
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends AbstractPage {
+
+    private static final Logger LOGGER = Logger.getLogger(LoginPage.class);
 
     @FindBy(xpath = "//div[@class='main main-review l-box col float-right']//h3")
     private ExtendedWebElement loginResult;
@@ -18,10 +21,12 @@ public class LoginPage extends AbstractPage {
     }
 
     public String getLoginStatus() {
+        LOGGER.info("Get login status.");
         return loginResult.getText();
     }
 
     public String loginFailReason(){
+        LOGGER.info("Get login fail reason.");
         return loginFailReason.getText();
     }
 }
